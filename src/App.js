1129c9d41d0b6afc42b305c3484e6c3c41ba6f33
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2023-08-18 11:04:07 +0200

    Add session content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import React from "react";
 import "./styles.css";
 
-export default function App() {
+// function Animal(props) {
+
+//   return (
+//     <div>
+//       {props.sound}{" "}
+//       <span role="img" aria-label={props.name}>
+//         {props.emoji}
+//       </span>
+//     </div>
+//   );
+// }
+
+// function Animal(props) {
+//   // console.log(props);
+//   const { sound, emoji, name } = props;
+//   // console.log(sound);
+//   // console.log(emoji);
+//   // console.log(name);
+//   return (
+//     <div>
+//       {sound}{" "}
+//       <span role="img" aria-label={name}>
+//         {emoji}
+//       </span>
+//     </div>
+//   );
+// }
+
+function Animal({ sound, emoji, name, onPet, isHungry }) {
+  // function handlePet() {
+  //   console.log("Thank you " + sound);
+  // }
   return (
-    <div>
-      <Pet />
-      <Pet />
-      <Pet />
-    </div>
+    <button onClick={onPet} type="button">
+      {isHungry ? "Feed me!" : sound}{" "}
+      <span role="img" aria-label={name}>
+        {emoji}
+      </span>
+    </button>
   );
 }
 
-function Pet() {
+export default function App() {
+  function handlePet() {
+    console.log("Thank you!");
+  }
   return (
     <div>
-      Meow{" "}
-      <span role="img" aria-label="A cat">
-        🐈
-      </span>
+      <Animal sound="Meow" emoji="🐈" name="A cat" onPet={handlePet} isHungry />
+      <Animal sound="Wuff" emoji="🐶" name="A dog" onPet={handlePet} />
+      <Animal sound="Piep" emoji="🐁" name="A mouse" onPet={handlePet} />
     </div>
   );
 }
